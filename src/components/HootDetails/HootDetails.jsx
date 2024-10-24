24499bdcac25412950427856c2ad360b89ef39a3
norman8823 <norman8823@hotmail.com>
2024-10-24 11:22:10 -0400

    create comments

--- src/components/HootDetails/HootDetails.jsx
+++ src/components/HootDetails/HootDetails.jsx
@@ -1,55 +1,58 @@
-
-import { useState, useEffect, useParams } from 'react';
-import * as hootService from '../../services/hootService';
-
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import * as hootService from "../../services/hootService";
+import CommentForm from "../CommentForm/CommentForm";
 
 const HootDetails = (props) => {
-    if (!hoot) return <main>Loading...</main>;
-    const [hoot, setHoot] = useState(null)
-    const { hootId } = useParams();
+  const [hoot, setHoot] = useState(null);
+  const { hootid } = useParams();
 
-    
-useEffect(() => {
+  const handleAddComment = async (commentFormData) => {
+    const newComment = await hootService.createComment(hootid, commentFormData);
+    setHoot({ ...hoot, comments: [...hoot.comments, newComment] });
+    console.log("commentFormData", commentFormData);
+  };
+
+  useEffect(() => {
     const fetchHoot = async () => {
-      const hootData = await hootService.show(hootId);
-      console.log('hootData', hootData);
+      const hootData = await hootService.show(hootid);
+      console.log("hootData", hootData);
       setHoot(hootData);
     };
     fetchHoot();
-  }, [hootId]);
-console.log('hoot state:', hoot);
-
-    return (
-        <main>
-          <header>
-            <p>{hoot.category.toUpperCase()}</p>
-            <h1>{hoot.title}</h1>
-            <p>
-              {hoot.author.username} posted on
-              {new Date(hoot.createdAt).toLocaleDateString()}
-            </p>
-          </header>
-          <p>{hoot.text}</p>
-          <section>
-            <h2>Comments</h2>
-            {!hoot.comments.length && <p>There are no comments.</p>}
+  }, [hootid]);
+  console.log("hoot state:", hoot);
+  if (!hoot) return <main>Loading...</main>;
+  return (
+    <main>
+      <header>
+        <p>{hoot.category.toUpperCase()}</p>
+        <h1>{hoot.title}</h1>
+        <p>
+          {hoot.author.username} posted on
+          {new Date(hoot.createdAt).toLocaleDateString()}
+        </p>
+      </header>
+      <p>{hoot.text}</p>
+      <section>
+        <h2>Comments</h2>
+        <CommentForm handleAddComment={handleAddComment} />
+        {!hoot.comments.length && <p>There are no comments.</p>}
 
         {hoot.comments.map((comment) => (
-         <article key={comment._id}>
-          <header>
-           <p>
-               {comment.author.username} posted on
-              {new Date(comment.createdAt).toLocaleDateString()}
-            </p>
-           </header>
-           <p>{comment.text}</p>
-         </article>
+          <article key={comment._id}>
+            <header>
+              <p>
+                {comment.author.username} posted on
+                {new Date(comment.createdAt).toLocaleDateString()}
+              </p>
+            </header>
+            <p>{comment.text}</p>
+          </article>
         ))}
-          </section>
-        </main>
-      );
-  };
+      </section>
+    </main>
+  );
+};
 
-
-  export default HootDetails;
-  
+export default HootDetails;
--- src/services/hootService.js
+++ src/services/hootService.js
@@ -41,6 +41,26 @@ const show = async (hootId) => {
     }
   };
 
+  const createComment = async (hootId, commentFormData) => {
+    try {
+      const res = await fetch(`${BASE_URL}/${hootId}/comments`, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(commentFormData),
+      });
+      return res.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  export {
+    index,
+    show,
+    create,
+    createComment,
+  };
 
-
-  export { index, show, create };
